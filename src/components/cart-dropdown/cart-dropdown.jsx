e0c159a7a910835dfd7ab0350d64d7c2748916fa
Abhi09jeet2000 <abhirbkulkarni@gmail.com>
2021-10-06 15:44:19 +0530

    handling checkout button

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -6,9 +6,8 @@ import './cart-dropdown.scss'
 
 const CartDropDown = () => (
   <div className="cart-dropdown">
-    <div className="cart-items">
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
+    <div className="cart-items"></div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
