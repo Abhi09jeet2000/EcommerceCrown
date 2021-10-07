9eb7b3ff5c585f180f3ac32b448cf0c7668a746b
Abhi09jeet2000 <abhirbkulkarni@gmail.com>
2021-10-07 18:15:58 +0530

    Added increasing, decreasing and removing feature in checkout page

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -33,7 +33,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total}</span>
+      <span>TOTAL: Rs{total}</span>
     </div>
   </div>
 )
